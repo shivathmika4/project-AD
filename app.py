daeb6261376cde6f21b951b21b4885646765ecbe
Shivathmika Vuppugandla <shivathmikavuppugandla@gmail.com>
2025-02-08 12:25:33 +0530

    project

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask,render_template
-app=Flask(__name__)
-@app.route('/')
-def Home():
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
     return render_template("index.html")
 
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        msg = request.form.get("message")  # Get user input
+        print(f"User Input: {msg}")  # Print to Visual Studio command shell
+        return render_template("result.html", message=msg)  # Redirect to result page
 
-if __name__=="__main__":
-    app.run()
+    return render_template("predict.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050, debug=True)
