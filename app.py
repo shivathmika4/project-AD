95c2dd11d7f4b94d853a3a2044e5226c9087f482
Sakre Abhinay <abhinaysakre7@gmail.com>
2025-02-14 10:01:09 +0530

    day5

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
-from flask import Flask, render_template , request
-app = Flask(__name__)
+from flask import Flask,render_template, request, jsonify
+from test import TextToNum
+import pickle
+app=Flask(__name__)
 @app.route("/")
 def Home():
     return render_template("index.html")
 @app.route("/predict",methods=["GET","POST"])
 def predict():
     if request.method == "POST":
-        msg= request.form.get("message")
+        msg=request.form.get("message")
         print(msg)
 
-    else:
-        return render_template("predict.html")
+        cl=TextToNum(msg)
+        cl.cleaner()
+        cl.token()
+        cl.removeStop()
+        st=cl.stemme()
+        stvc=" ".join(st)
+        with open("vectorizer.pickle","rb") as vc_file:
+            vectorizer=pickle.load(vc_file)
+        dt=vectorizer.transform([stvc]).toarray()
+        with open('model.pickle',"rb") as md_file:
+            model=pickle.load(md_file)
+        pred=model.predict(dt)
+        if pred[0]==1:
+            pred="Positive"
+        elif pred[0]==-1:
+            pred="Negative" 
+        else:
+            pred="Neutral"       
+       
+        return render_template("result.html",prediction=pred)
 
+
+    return render_template("predict.html")
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=5050)
